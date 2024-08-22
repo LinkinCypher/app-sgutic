17f5757a2c6d9e8b7910c53dd37a44ea92760b94
Cristhian Mora <linkincypher@hotmail.com>
2024-08-22 11:46:42 -0500

    configuración de las variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que las variables de entorno estén disponibles en toda la aplicación
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI), // Conecta a MongoDB usando la URI desde las variables de entorno
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

3264ca4c9e2c7bf91fac49c71babc650a883c22e
Cristhian Mora <linkincypher@hotmail.com>
2024-08-27 10:38:14 -0500

    agregando comentarios

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secretKey',  //coincidir con el valor de `secret` en `JwtModule`
+      secretOrKey: 'secretKey',
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,11 @@ async function bootstrap() {
   // Habilitar CORS para permitir solicitudes desde otros orígenes
   app.enableCors({
     origin: 'http://localhost:8100', // Permite solo solicitudes desde la aplicación Ionic
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Permitir estos métodos HTTP
+    credentials: true, // Permitir el uso de cookies o autenticación basada en sesiones
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3000; // Usa el puerto desde .env o el 3000 por defecto
   await app.listen(port);
   console.log(`La aplicación se está ejecutando en: http://localhost:${port}`);
 }
